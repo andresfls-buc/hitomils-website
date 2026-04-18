4a6523e15f860bd58dd71a6c44c4ba66f474e978
Andres Landazabal <andresflsxx@gmail.com>
2026-04-18 09:48:48 +0900

    add: new blog post for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,6 +115,15 @@ export default function RootLayout({
             gtag('config', 'G-5B5KJV854H');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wdcr9p47cd");
+          `}
+        </Script>
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
